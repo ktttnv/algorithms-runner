6696dca231a407feb62d948cf81edec812b3b531
Ekaterina Tatanova <e.tatanova@mail.ru>
2021-04-01 01:29:50 +0300

    Cleaned up and updated Home page

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <img alt="Header image" src="../assets/header.jpg" height="350"/>
+    <HelloWorld msg="Welcome to Algorithm Runner App" />
   </div>
 </template>
 
