f62fd7778a22dc7ce213575f4f12e2faae560b7d
Ekaterina Tatanova <e.tatanova@mail.ru>
2021-04-03 20:10:10 +0300

    Added Footer Component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -6,16 +6,19 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view /> -->
+    <Footer msg="Hi, I'm a footer" />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header/Header.vue";
+import Footer from "./components/Footer/Footer.vue";
 
 export default {
   name: "App",
   components: {
     Header,
+    Footer,
   },
 };
 </script>
@@ -24,7 +27,9 @@ export default {
 .container {
   width: 960px;
   margin: 0 auto;
+  background-color: rgb(230, 248, 255);
 }
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ frontend/src/components/Footer/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="footer">
+    <h3>{{ msg }}</h3>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+
+@Options({
+  props: {
+    msg: String,
+  },
+})
+export default class Footer extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  color: #409dfe;
+}
+</style>
